cd2cc47d59f674d29e74861d65faaf8d84bceb1a
Nicolas Bartoli <nbartoli@mblspn.com>
2012-09-14 18:16:43 -0700

    Global improvement
    - Added missing templatetags filter
    - Improved login page
    - fixed some padding issue for mobile/tablet display

---
+++ nexus/templatetags/custom_form.py
@@ -0,0 +1,33 @@
+import re
+from django.conf import settings
+
+import urllib, hashlib
+
+from django import template
+from django.contrib.sites.models import Site
+from django.utils.safestring import mark_safe
+
+
+
+
+register = template.Library()
+@register.filter(name='label_with_classes')
+def label_with_classes(value, arg):
+    return value.label_tag(attrs={'class': arg})
+
+class_re = re.compile(r'(?<=class=["\'])(.*)(?=["\'])')
+@register.filter
+def add_class(value, css_class):
+    string = unicode(value)
+    match = class_re.search(string)
+    if match:
+        m = re.search(r'^%s$|^%s\s|\s%s\s|\s%s$' % (css_class, css_class, 
+                                                    css_class, css_class), 
+                      match.group(1))
+        print match.group(1)
+        if not m:
+            return mark_safe(class_re.sub(match.group(1) + " " + css_class, 
+                                          string))
+    else:
+        return mark_safe(string.replace('>', ' class="%s">' % css_class))
+    return value
